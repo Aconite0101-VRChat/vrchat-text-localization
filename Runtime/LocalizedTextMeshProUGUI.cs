3c0fed1e17ab6e561b53a7adc36cb287e169507d
IkaWaAyuMu <pungpond4450@gmail.com>
2025-12-28 23:09:47 +0700

    feat: TextMeshProUGUI application

---
+++ Runtime/LocalizedTextMeshProUGUI.cs
@@ -0,0 +1,23 @@
+﻿
+using TMPro;
+using UnityEngine;
+
+namespace TextLocalization
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class LocalizedTextMeshProUGUI : LocalizedText
+    {
+        private TextMeshProUGUI tc;
+        void Start()
+        {
+            tc = GetComponent<TextMeshProUGUI>();
+        }
+
+        public override void OnLanguageChanged(string language)
+        {
+            base.OnLanguageChanged(language);
+
+            tc.text = base.GetTextToChanged(language) ?? tc.text;
+        }
+    }
+}
