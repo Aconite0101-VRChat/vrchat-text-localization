4a61c17a0dbddc33be617c582d861aa6a1b66be8
IkaWaAyuMu <pungpond4450@gmail.com>
2025-12-28 23:09:17 +0700

    feat: Legacy UI text application

---
+++ Runtime/LocalizedTextUI.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TextLocalization
+{
+    [RequireComponent(typeof(Text))]
+    public class LocalizedTextUI : LocalizedText
+    {
+        private Text tc;
+        void Start()
+        {
+            tc = GetComponent<Text>();
+        }
+
+        public override void OnLanguageChanged(string language)
+        {
+            base.OnLanguageChanged(language);
+
+            tc.text = base.GetTextToChanged(language) ?? tc.text;
+        }
+    }
+}
