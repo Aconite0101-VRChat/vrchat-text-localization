adcecad751c35acb951170a18777f07e69578feb
IkaWaAyuMu <pungpond4450@gmail.com>
2025-12-28 23:10:01 +0700

    feat: TextMeshPro application

---
+++ Runtime/LocalizedTextMeshPro.cs
@@ -0,0 +1,23 @@
+﻿
+using TMPro;
+using UnityEngine;
+
+namespace TextLocalization
+{
+    [RequireComponent(typeof(TextMeshPro))]
+    public class LocalizedTextMeshPro : LocalizedText
+    {
+        private TextMeshPro tc;
+        void Start()
+        {
+            tc = GetComponent<TextMeshPro>();
+        }
+
+        public override void OnLanguageChanged(string language)
+        {
+            base.OnLanguageChanged(language);
+
+            tc.text = base.GetTextToChanged(language) ?? tc.text;
+        }
+    }
+}
